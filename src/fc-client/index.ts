3b9180d354aa110187e8c187ea8e85502e2e1e08
xsahxl <xsahxl@126.com>
2022-03-30 16:26:02 +0800

    feat: 🎸 add getCredentialWithAcc

--- src/fc-client/index.ts
+++ src/fc-client/index.ts
@@ -1,16 +1,62 @@
 import _ from "lodash";
 import FC from "@alicloud/fc2";
-import { getCredential } from '@serverless-devs/core';
+import * as core from "@serverless-devs/core";
 import { CatchableError } from "../utils/errors";
 import { checkEndpoint, getEndpointFromFcDefault } from "./endpoint";
 import logger from "../utils/logger";
 import { MakeFcClientInput } from "../utils/interface";
 import { ICredentials } from "../interface";
-import './fc-client';
+import "./fc-client";
+import Acc from "@serverless-devs/acc/commands/run";
+import path from "path";
+import os from "os";
+export { getEndpointFromFcDefault } from "./endpoint";
 
-export { getEndpointFromFcDefault } from './endpoint';
-
+const { fse: fs } = core;
 const DEFAULT_TIMEOUT = 600;
+const ALIYUN_CONFIG_FILE = path.join(os.homedir(), ".aliyun", "config.json");
+
+/**
+ * 获取密钥，包含s和aliyun-cli
+ * @param access: 密钥别名
+ * @returns { AccountID: string, AccessKeyID: string, AccessKeySecret: string, SecurityToken?: string}
+ */
+
+export async function getCredentialWithAcc(access: string) {
+  const data = await core.getCredentialAliasList();
+  if (_.includes(data, access)) {
+    return await core.getCredential(access);
+  }
+  const configPath = process.env.ALIBABACLOUD_CONFIG || ALIYUN_CONFIG_FILE;
+  if (fs.existsSync(configPath)) {
+    let accData;
+    try {
+      accData = await new Acc().run([]);
+    } catch (error) {}
+    if (accData?.AccessKeyID && accData?.AccessKeySecret) {
+      const stockData = core.getConfig("acc");
+      const findObj = _.find(
+        stockData,
+        (o) => o.AccessKeyID === accData.AccessKeyID
+      );
+      if (findObj) {
+        accData.AccountID = findObj.AccountId;
+        return accData;
+      }
+      const info: any = await core.getAccountId(accData);
+      const tmp = [
+        {
+          AccountId: info.AccountId,
+          AccessKeyID: accData.AccessKeyID,
+        },
+      ];
+      core.setConfig("acc", stockData ? _.concat(stockData, tmp) : tmp);
+      accData.AccountID = info.AccountId;
+      return accData;
+    }
+  }
+  return await core.getCredential(access);
+}
 
 /**
  * 获取 fc client
@@ -28,7 +74,7 @@ export async function makeFcClient(props: MakeFcClientInput) {
 
   let credentials: ICredentials;
   if (_.isEmpty(props.credentials)) {
-    const credentialRes: any = await getCredential(props.access);
+    const credentialRes: any = await getCredentialWithAcc(props.access);
     credentials = {
       AccountID: credentialRes?.AccountID,
       AccessKeyID: credentialRes?.AccessKeyID,
